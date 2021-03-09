5d4a5fd8b043de74b8162b6faacf4d6e23a6ed2b
Jon Edvald <edvald@gmail.com>
2021-03-09 23:43:28 +0100

    Don't run containers on port 80

--- api/app.py
+++ api/app.py
@@ -62,4 +62,4 @@ if __name__ == "__main__":
             labels={ "env": "dev", "service": "api", "instance_id": instance_id },
         )
 
-    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
--- vote/tests/integ/test.js
+++ vote/tests/integ/test.js
@@ -2,20 +2,20 @@ const axios = require('axios');
 const { expect } = require('chai');
 
 describe('GET /', () => {
-  it('respond with 200', async () => {
-    const result = await axios.get('http://vote:8080', {});
+  it('should respond with 200', async () => {
+    const result = await axios.get('http://vote', {});
     expect(result.status).to.eql(200);
   });
 });
 
 describe('POST /api/vote', () => {
-  it('respond with 200', async () => {
+  it('should respond with 200', async () => {
     const headers = {
       'Content-Type': 'application/x-www-form-urlencoded',
       'Access-Control-Allow-Origin': '*',
     };
     const voteResult = 'a';
-    const result = await axios.post('http://vote:8080/api/vote', `vote=${voteResult}`, { headers });
+    const result = await axios.post('http://vote/api/vote', `vote=${voteResult}`, { headers });
     expect(result.status).to.eql(200);
   });
 });
